4f6868b45e068edf87c4359d8897af4e1c2656b3
sanmoyano <smoyano.di@gmail.com>
2023-05-24 20:19:20 -0300

    styling navigation

--- src/navigation/tabs/index.jsx
+++ src/navigation/tabs/index.jsx
@@ -1,3 +1,4 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
 
@@ -26,17 +27,34 @@ const TabNavigator = () => {
         component={ShopNavigator}
         options={{
           tabBarLabel: "Shop",
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons size={size} color={color} name={focused ? "home" : "home-outline"} />
+          ),
         }}
       />
       <BottomTab.Screen
         name="OrdersTab"
         component={OrdersNavigator}
-        options={{ tabBarLabel: "Orders" }}
+        options={{
+          tabBarLabel: "Orders",
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              size={size}
+              color={color}
+              name={focused ? "file-tray" : "file-tray-outline"}
+            />
+          ),
+        }}
       />
       <BottomTab.Screen
         name="CartTab"
         component={CartNavigator}
-        options={{ tabBarLabel: "Cart" }}
+        options={{
+          tabBarLabel: "Cart",
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons size={size} color={color} name={focused ? "cart" : "cart-outline"} />
+          ),
+        }}
       />
     </BottomTab.Navigator>
   );
