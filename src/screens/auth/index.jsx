7a2e9c55a9dd1a12777e8a90ed6cc1059e201b31
sanmoyano <smoyano.di@gmail.com>
2023-06-08 17:40:43 -0300

    log ui

--- src/screens/auth/index.jsx
+++ src/screens/auth/index.jsx
@@ -1,12 +1,50 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, TouchableOpacity } from "react-native";
 
 import { styles } from "./styles";
+import { theme } from "../../constants";
 
 const Auth = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const title = isLogin ? "Login" : "Register";
+  const buttonTitle = isLogin ? "Login" : "Register";
+  const messageText = isLogin ? "Don't have an account?" : "Already have an account";
+
+  const onHandleOnchageAuth = () => {
+    setIsLogin(!isLogin);
+  };
+
   return (
     <View style={styles.container}>
-      <Text> Auth</Text>
+      <View style={styles.content}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.label}>Email</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="email@gmail.com"
+          placeholderTextColor={theme.colors.transparent}
+          autoCapitalize="none"
+          autoCorrect={false}
+          onChange={() => null}
+        />
+        <Text style={styles.label}>Password</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="*******"
+          placeholderTextColor={theme.colors.transparent}
+          autoCapitalize="none"
+          autoCorrect={false}
+          onChange={() => null}
+        />
+        <View style={styles.linkContainer}>
+          <TouchableOpacity style={styles.link} onPress={onHandleOnchageAuth}>
+            <Text style={styles.linkText}>{messageText}</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.submit}>
+          <Button title={buttonTitle} onPress={() => null} />
+        </View>
+      </View>
     </View>
   );
 };
